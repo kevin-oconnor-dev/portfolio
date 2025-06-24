c5b1b4b927badc1fe7187595028382534695516b
Kevin O'Connor <197013454+kevin-oconnor-dev@users.noreply.github.com>
2025-06-24 17:24:24 -0400

    Use history.back and only fade-in skills once

--- app.js
+++ app.js
@@ -6,29 +6,40 @@ const skillsBox = document.querySelector('#skills-box');
 
 const observer = new IntersectionObserver((entries, observer) => {
   const entry = entries[0]; // only one element observed
-  if (entry.isIntersecting) {
+  let seen = sessionStorage.getItem('seenFadeIn');
+  if (entry.isIntersecting && seen !== 'true') {
     fadeInSkills();
+    sessionStorage.setItem('seenFadeIn', 'true')
     observer.unobserve(entry.target); // stop listening after first trigger
   }
 });
 observer.observe(skillsBox);
+if (sessionStorage.getItem('seenFadeIn') === 'true') {
+  showSkillsImmediately();
+}
 
 let headerHeight = headerElement.offsetHeight;
 mainHeaderStroke.style.top = headerHeight / 1/3 + 'px';
 mainHeaderFill.style.top = headerHeight / 1/3 + 'px';
 
 function fadeInSkills() {
-    const skills = document.querySelectorAll('.skill');
-    skillsHeader.className = '';
+  const skills = document.querySelectorAll('.skill');
+  skillsHeader.className = '';
 
-    for (let i = 0; i < skills.length; i++) {
-        let delay = 250 * i + 100;
-        setTimeout( () => {
-            skills[i].style.opacity = '1'
-        }, delay)
-    }
-    let endTime = 250 * (skills.length - 1) + 600;
-    setTimeout(() => {
-      skillsHeader.className = 'animated';
-    }, endTime)
+  for (let i = 0; i < skills.length; i++) {
+    let delay = 250 * i + 100;
+    setTimeout( () => {
+      skills[i].style.opacity = '1'
+    }, delay)
+  }
+  let endTime = 250 * (skills.length - 1) + 600;
+  setTimeout(() => {
+    skillsHeader.className = 'animated';
+  }, endTime)
+}
+
+function showSkillsImmediately() {
+  document.querySelectorAll('.skill').forEach(skill => {
+    skill.style.opacity = 1;
+  })
 }
